a57847a3a410b6698a33da746247475fbe588a89
Ricardo <ederli.picinatto@gmail.com>
2020-06-21 10:41:56 -0300

    added fonts to the project and styled SignIn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import SignIn from '../Pages/SignIn';
-import SignUp from '../Pages/SignUp';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Auth = createStackNavigator();
 
