06125b726fa9c7c80cf7482452a9e356794e7856
Ricardo <ederli.picinatto@gmail.com>
2020-06-27 11:14:52 -0300

    add logic to buttons

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -9,15 +10,26 @@ interface InputProps extends TextInputProps {
   icon: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
-    <TextInput
-      keyboardAppearance="dark"
-      placeholderTextColor="#666360"
-      {...rest}
-    />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+  const inputRef = useRef(null);
+
+  const { registerField, defaultValue, fieldName, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+    });
+  });
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+      <TextInput
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        {...rest}
+      />
+    </Container>
+  );
+};
 
 export default Input;
